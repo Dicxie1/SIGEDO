aab1d1510e4ad1a22f29b51121d25098bf4ada27
Dicxie Madrigal <dicxiemadrigal@gmail.com>
2026-01-30 16:25:06 -0600

    feat: add the Teacher model

---
+++ Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace Asistencia.Models
+{
+    public class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Sex Sex { get; set; }
+    }   
+}
