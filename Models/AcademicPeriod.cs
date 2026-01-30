d084a79910cf69e34cffb13bf320733f6c542e4b
Dicxie Madrigal <dicxiemadrigal@gmail.com>
2026-01-30 14:29:40 -0600

    feat: add academicPeriod Model

---
+++ Models/AcademicPeriod.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Asistencia.Models
+{
+    public enum EnumPeriodStatus {
+    Closed = 0,
+    Active = 1,
+}
+    public class AcademicPeriod
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly StartPeriod { get; set; }
+        public DateOnly ENdPeriodñ { get; set; }
+        public EnumPeriodStatus Status { get; set; }
+    }
+}
